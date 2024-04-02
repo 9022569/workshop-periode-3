95fa695bc98929452154b7fde5f6ac18f1aa018d
Ethan <9018008@student.zadkine.nl>
2024-04-02 16:21:19 +0200

    navmenu v1

--- Pages/Index.php
+++ Pages/Index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index.php</title>
+    <link rel="stylesheet" href="navmenu.css">
 </head>
 <body>
-    
+    <header class="nav">
+        <?php include "navmenu.php"; ?>
+    </header>
 </body>
 </html>
