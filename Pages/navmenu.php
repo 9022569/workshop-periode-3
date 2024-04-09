9bcfdea3c5c80bef8b9167ec2042bee58dd36576
Ethan <9018008@student.zadkine.nl>
2024-04-09 12:05:07 +0200

    navmenu v2

--- Pages/navmenu.php
+++ Pages/navmenu.php
@@ -1,16 +1,16 @@
 <nav class="nav">
     <ul>
         <li><a href="index.php">Home Pagina</a></li>
-        <li><a href="page1.php">Tabel pagina[verzin goeie naam]</a></li>
-        <li><a href="page2.php">FAQ</a></li>
-        <li><a href="page3.php">Info over bedrijf</a></li>
+        <li><a href="tabellen.php">Tabellen lijst</a></li>
+        <li><a href="FAQ.php">FAQ</a></li>
+        <li><a href="info.php">Info over bedrijf</a></li>
         <li class="dropdown">
             <button class="dropbtn">Klachten of complimenten</button>
             <div class="dropdown-content">
-                <a href="page4.php">Complimenteren</a>
-                <a href="page5.php">Klacht over website</a>
-                <a href="page6.php">Klacht over product</a>
-                <a href="page7.php">Klacht over medewerker</a>
+                <a href="compliment.php">Complimenteren</a>
+                <a href="klachtweb.php">Klacht over website</a>
+                <a href="klachtproduct.php">Klacht over product</a>
+                <a href="klachtwerker.php">Klacht over medewerker</a>
             </div>
         </li>
     </ul>
