a0dad712477d91c77428361c6770e7cda0ed9d1c
Ethan <9018008@student.zadkine.nl>
2024-04-13 22:46:10 +0200

    klein fix

--- Pages/medewerkers.php
+++ Pages/medewerkers.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="navmenu.css">
+    <link rel="stylesheet" href="Style.css">
     <link rel="stylesheet" href="medewerkers.css">
 </head>
+<header>
+    <nav>
+        <?php include "../Include pages/navmenu.php"; ?>
+    </nav>
+</header>
 <body>
-    <header class="nav">
-        <?php include "navmenu.php"; ?>
-    </header>
 
     <section class="medewerkers">
 
