afc61939c76925d8b78289e65a334840d27c21be
Ethan <9018008@student.zadkine.nl>
2024-04-14 19:32:21 +0200

    medewerkers v1

--- Include pages/navmenu.php
+++ Include pages/navmenu.php
@@ -4,7 +4,7 @@
         <li><a href="tabellen.php">Tabellen lijst</a></li>
         <li><a href="Producten.php">Bekijk al onze producten!</a></li>
         <li><a href="FAQ.php">FAQ</a></li>
-        <li><a href="medewerkers.php">Medewerkers</a></li>
+        <li><a href="medewerkers.php">Belangrijke medewerkers</a></li>
         <li><a href="info.php">Info over bedrijf</a></li>
         <li class="dropdown">
             <button class="dropbtn">Klachten of complimenten</button>
