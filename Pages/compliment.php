91d898c31616ce6edb79d3945a03379f62da6502
Ethan <9018008@student.zadkine.nl>
2024-04-14 21:04:49 +0200

    compliment form v1

---
+++ Pages/compliment.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="compliment.css">
+</head>
+<body>
+<header>
+    <nav>
+        <?php include "../Include pages/navmenu.php"; ?>
+    </nav>
+</header>
+
+<main class="main-form">
+    <div class="form-container">
+        <form class="form" action="" method="post">
+            <label for="name">Naam:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="message">Bericht:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <input type="submit" value="Verstuur">
+        </form>
+    </div>
+</main>
+
+</body>
+</html>
