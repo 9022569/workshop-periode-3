d6ffd5866f5a63cdc3a1d8e0470764262a4b1a1f
emir <144111368+9022569@users.noreply.github.com>
2024-04-15 12:55:27 +0200

    Add files via upload

---
+++ Pages/homepage3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mijn Homepage</title>
+    <link rel="stylesheet" href="style doel.css">
+</head>
+<body>
+    <div class="container">
+        <div class="search-container">
+            <form action="search.php" method="GET">
+                <input type="text" name="query" class="search-bar" placeholder="Zoek hier...">
+                <input type="submit" value="Zoeken" class="button">
+            </form>
+        </div>
+        <div class="boxes-container">
+            <div class="box">
+                
+            </div>
+            <div class="box">
+               
+            </div>
+            <div class="box">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
